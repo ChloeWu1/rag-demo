efbe23057029092ee762cac0df4059ad4dbde07c
ChloeWu1 <jiawei.wu@intel.com>
2024-04-25 16:45:52 +0800

    add markdown

--- run_rag.py
+++ run_rag.py
@@ -426,6 +426,7 @@ with gr.Blocks(
 ) as demo:
     gr.Markdown("""<h1><center>QA over Document</center></h1>""")
     gr.Markdown(f"""<center>Powered by OpenVINO and {llm_model_id} </center>""")
+    gr.Markdown(f"""<center>MeteorLake Optimized by OpenVINO </center>""")
 
     # Insert logo image in the top left corner
     demo.top_left = gr.Image(value="utils/logo_trans.png", width=100, height=100, interactive=False, show_share_button=False, show_label=False, show_download_button=False)
